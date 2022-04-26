d1ca4c43e1ed75690efd9c7f2514983b8ea4cb80
puja <pujarana1796@gmail.com>
2022-04-26 12:33:30 +0530

    parttime

--- employeeWage/Program.cs
+++ employeeWage/Program.cs
@@ -8,11 +8,30 @@ namespace employeeWage
         {
 
             Console.WriteLine("Welcome to employee wage program!");
-            presentORabsent p1 = new presentORabsent();
-            p1.present();
-            //Wagecalculation
-            Wagecalculation w1 = new Wagecalculation();
-            w1.calculation();
+            while (true)
+            {
+                Console.WriteLine("select number\n1)check present or bsent\n2)wage computation\n3) part time wage compuatation\n ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        presentORabsent p1 = new presentORabsent();
+                        p1.present();
+                        break;
+
+                    //Wagecalculation
+                    case 2:
+
+                        Wagecalculation w1 = new Wagecalculation();
+                        w1.calculation();
+                        break;
+                    //ParttimeEmployee
+                    case 3:
+                        Parttimeemployee part = new Parttimeemployee();
+                        part.partTime();
+                        break;
+                }
+            }
         }
     }
 }
